fe84a8afe39516b6cdce1637a2d7cd5e24ca07e4
James Wong <jwong_li@hotmail.com>
2020-04-14 00:44:19 -0700

    Add Star icons on top of the movie thumbnails

--- src/app/directives/app.movieImages.ts
+++ src/app/directives/app.movieImages.ts
@@ -2,18 +2,22 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'images-directive',
-    template: `<img src='{{posterPath}}' alt={{altText}}/>`,
-    styles: [`
-        img { 
-            width: 185px; height: 277px; 
-            border-radius: 9px 9px 0 0;
-        }
-    `]
+    template: `
+        <img src='{{posterPath}}' alt={{altText}}/>
+        <div class="star-rating">
+            <img class="star" src="../../assets/images/star.svg" alt="Star rating">
+            <span>{{rating}}</span>
+        </div>`,
+    styleUrls: ['app.movieImages.scss']
 })
+
 export class MovieImages {
     @Input()
     posterPath: string;
 
     @Input()
-    altText: string
+    altText: string;
+
+    @Input()
+    rating: string;
 }
