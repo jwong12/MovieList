d2cefa6d9e4a537d5f106fcc735ec3263ca14504
James Wong <jwong_li@hotmail.com>
2020-04-14 02:00:17 -0700

    Fix nav bar interactivity issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-    
+    searchKeywords: string;
+
+    search() {
+        console.log(this.searchKeywords.trim());
+    }
 }
