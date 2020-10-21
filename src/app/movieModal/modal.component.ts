d9e9620785ebb2b52c206e7efb1fdf87c912aa59
James Wong <jwong_li@hotmail.com>
2020-10-21 12:28:33 -0700

    Use ViewChild decorators to access modalBody's div and saveButton's span elements

--- src/app/movieModal/modal.component.ts
+++ src/app/movieModal/modal.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, ViewEncapsulation, ElementRef, Input, OnInit, OnDestroy } from '@angular/core';
+﻿import { Component, ViewEncapsulation, ElementRef, Input, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Router } from "@angular/router"
 import { ModalService } from './modal.service';
 import { APIService } from '../API.service';
@@ -24,7 +24,6 @@ export class ModalComponent implements OnInit, OnDestroy {
     userRating: number;
     popularity: number;
     overview: string;
-    saveButton: HTMLElement;
     userAuthenticated:boolean = false;
     movieIsInDB: boolean;
     imageLoaded: boolean = false;
@@ -33,6 +32,12 @@ export class ModalComponent implements OnInit, OnDestroy {
     @Input() id: string;
     private element: any;
 
+    @ViewChild('modalBody', {static: false}) 
+    modalBodyEl: ElementRef; 
+
+    @ViewChild('saveButton', {static: false}) 
+    saveButtonEl: ElementRef; 
+    
     constructor(private router: Router, private modalService: ModalService, private el: ElementRef, private api:APIService, private spinner: NgxSpinnerService) {
         this.element = el.nativeElement;
     }
@@ -71,10 +76,9 @@ export class ModalComponent implements OnInit, OnDestroy {
     // open modal
     open(movie, genreArray): void {
         this.element.style.display = 'block';
-        this.element.childNodes[0].childNodes[0].style.display = 'none';
+        this.modalBodyEl.nativeElement.style.display = 'none';
         this.spinner.show();
         document.body.classList.add('jw-modal-open');
-        this.saveButton = this.element.childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].childNodes[1];
         this.imgSrc = posterLink + movie.poster_path;
         this.imgSrcLow = posterLinkLow + movie.poster_path;
         this.title = movie.title;
@@ -94,12 +98,13 @@ export class ModalComponent implements OnInit, OnDestroy {
 
             } else {
                 this.movieIsInDB = false;
-                this.setSaveButton();
+                this.setsaveButtonEl();
             }
 
             if (this.imageLoaded) {
                 this.spinner.hide();
             }
+
             this.receiveMovieFromApi = true;
 
         }).catch(() => {});  
@@ -109,8 +114,9 @@ export class ModalComponent implements OnInit, OnDestroy {
         if (this.receiveMovieFromApi) {
             this.spinner.hide();
         }
+
         this.imageLoaded = true;
-        this.element.childNodes[0].childNodes[0].style.display = 'flex';
+        this.modalBodyEl.nativeElement.style.display = 'flex';
     }
 
     cancelModal() {
@@ -118,18 +124,18 @@ export class ModalComponent implements OnInit, OnDestroy {
         this.close();
     }
 
-    setSaveButton() {
-        this.saveButton.textContent = "Save";
-        this.saveButton.style.backgroundColor = "rgba(204, 127, 46, 0.93)";
-        this.saveButton.style.border = "solid 1px rgba(204, 127, 46, 0.93)";
-        this.saveButton.style.color = "#fbfbfb";
+    setsaveButtonEl() {
+        this.saveButtonEl.nativeElement.textContent = "Save";
+        this.saveButtonEl.nativeElement.style.backgroundColor = "rgba(204, 127, 46, 0.93)";
+        this.saveButtonEl.nativeElement.style.border = "solid 1px rgba(204, 127, 46, 0.93)";
+        this.saveButtonEl.nativeElement.style.color = "#fbfbfb";
     }
 
     setAddedButton() {
-        this.saveButton.textContent = "Added";
-        this.saveButton.style.backgroundColor = "rgba(130, 130, 130, 0.93)";
-        this.saveButton.style.border = "solid 1px rgba(130, 130, 130, 0.93)";
-        this.saveButton.style.color = "#ffffff";
+        this.saveButtonEl.nativeElement.textContent = "Added";
+        this.saveButtonEl.nativeElement.style.backgroundColor = "rgba(130, 130, 130, 0.93)";
+        this.saveButtonEl.nativeElement.style.border = "solid 1px rgba(130, 130, 130, 0.93)";
+        this.saveButtonEl.nativeElement.style.color = "#ffffff";
     }
 
     async addToWatchList() {
@@ -139,7 +145,7 @@ export class ModalComponent implements OnInit, OnDestroy {
 
         } else if(this.movieIsInDB) {
             this.movieIsInDB = false;
-            this.setSaveButton();
+            this.setsaveButtonEl();
 
             const movie = {
                 id: this.title
@@ -174,11 +180,9 @@ export class ModalComponent implements OnInit, OnDestroy {
                 if(genreArray[j].id === genreId[i]) {
                     if(genres === '') {
                         genres += genreArray[j].name
-
                     } else {
                         genres += ', ' + genreArray[j].name
                     }
-                    
                     break;
                 }
             }
@@ -194,12 +198,12 @@ export class ModalComponent implements OnInit, OnDestroy {
         this.imageLoaded = false;
         this.receiveMovieFromApi = false;
 
-        if(this.saveButton) {
-            this.saveButton.textContent = "Save";
-            this.saveButton.style.backgroundColor = "rgba(204, 127, 46, 0.93)";
-            this.saveButton.style.border = "solid 1px rgba(204, 127, 46, 0.93)";
-            this.saveButton.style.color = "#fbfbfb";
-            this.saveButton.style.cursor = "pointer";
+        if(this.saveButtonEl) {
+            this.saveButtonEl.nativeElement.textContent = "Save";
+            this.saveButtonEl.nativeElement.style.backgroundColor = "rgba(204, 127, 46, 0.93)";
+            this.saveButtonEl.nativeElement.style.border = "solid 1px rgba(204, 127, 46, 0.93)";
+            this.saveButtonEl.nativeElement.style.color = "#fbfbfb";
+            this.saveButtonEl.nativeElement.style.cursor = "pointer";
         }
     }
 }
