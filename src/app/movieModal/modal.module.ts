96b41cce62ad61a21c0a050bcb6a109188c5d44d
James Wong <jwong_li@hotmail.com>
2020-11-08 15:26:32 -0800

    Redesigned the progress circles in both the modal and watch list components

--- src/app/movieModal/modal.module.ts
+++ src/app/movieModal/modal.module.ts
@@ -9,24 +9,7 @@ import { NgxSpinnerModule } from "ngx-spinner";
     imports: [
         CommonModule, 
         NgxSpinnerModule,
-        NgCircleProgressModule.forRoot({
-            maxPercent: 100,
-            showSubtitle: false,
-            radius: 16,
-            titleFontSize: "13",
-            titleFontWeight: "600",
-            titleColor: "#3a3a3a",
-            unitsFontWeight: "600",
-            unitsColor: "#3a3a3a",
-            outerStrokeWidth: 4,
-            innerStrokeWidth: 4,
-            showInnerStroke: true,
-            backgroundStrokeWidth: 0,
-            backgroundPadding: 0,
-            animation: true,
-            animationDuration: 1100,
-            space: -4
-        })
+        NgCircleProgressModule.forRoot({})
     ],
     declarations: [ModalComponent],
     exports: [ModalComponent]
